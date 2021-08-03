c0e98bbbbb8eb57ca04551967df5b804a0f2d39f
nosteiner <no.steiner@gmail.com>
2021-08-03 15:35:04 +0300

    angular 11

--- src/app/components/single-site/single-site.component.spec.ts
+++ src/app/components/single-site/single-site.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SingleSiteComponent } from './single-site.component';
 
@@ -6,7 +6,7 @@ describe('SingleSiteComponent', () => {
   let component: SingleSiteComponent;
   let fixture: ComponentFixture<SingleSiteComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SingleSiteComponent ]
     })
